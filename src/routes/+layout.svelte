2fef7885b817fa2510e047c451140065c683813d
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-05-24 18:37:05 -0400

    Merge Conflict Settings

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+<<<<<<< HEAD
     import "@/lib/assets/sass/main.scss";
     // import { onMount } from "svelte";
 
@@ -72,6 +73,50 @@
         font-weight: bold;
     }
 
+=======
+</script>
+
+<div class="app">
+    <main>
+        <slot />
+    </main>
+
+    <footer>
+        <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+    </footer>
+</div>
+
+<style>
+    .app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    main {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        width: 100%;
+        max-width: 64rem;
+        margin: 0 auto;
+        box-sizing: border-box;
+    }
+
+    footer {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 12px;
+    }
+
+    footer a {
+        font-weight: bold;
+    }
+
+>>>>>>> 629cb9d (Created Necessary Files & Components, All Empty)
     @media (min-width: 480px) {
         footer {
             padding: 12px 0;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
+<<<<<<< HEAD
     import Navigation from "../lib/components/Navigation.svelte";
     import Header from "../lib/components/Header.svelte";
     import Main from "../lib/components/Main.svelte";
     import Aside from "../lib/components/Aside.svelte";
     import Categories from "../lib/components/Categories.svelte";
+=======
+>>>>>>> 629cb9d (Created Necessary Files & Components, All Empty)
 </script>
 
 <svelte:head>
@@ -11,6 +14,7 @@
     <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
+<<<<<<< HEAD
 <Navigation />
 <Header />
 <div class="body-tings">
@@ -42,5 +46,42 @@
         @include abstracts.breakpoint(1023) {
             flex-direction: column;
         }
+=======
+<section>
+    <h1>Svelte App</h1>
+
+    <h2>
+        try editing <strong>src/routes/+page.svelte</strong>
+    </h2>
+</section>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 0.6;
+    }
+
+    h1 {
+        width: 100%;
+    }
+
+    .welcome {
+        display: block;
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding: 0 0 calc(100% * 495 / 2048) 0;
+    }
+
+    .welcome img {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        display: block;
+>>>>>>> 629cb9d (Created Necessary Files & Components, All Empty)
     }
 </style>
