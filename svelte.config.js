52f6d5e880d5b877b3bfb8da433bf6715b5ccfcd
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-05-24 18:10:00 -0400

    Resolving Merging Conflicts

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
+<<<<<<< HEAD
 import adapter from "@sveltejs/adapter-netlify";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+=======
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+>>>>>>> 343699b (Setup Sass)
 import preprocessor from "svelte-preprocess";
 import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+<<<<<<< HEAD
     kit: {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -15,6 +21,18 @@ const config = {
         },
     },
     preprocess: preprocessor(vitePreprocess()),
+=======
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		alias: {
+			"@": path.resolve("./src")
+		}
+	},
+	preprocess: preprocessor(vitePreprocess())
+>>>>>>> 343699b (Setup Sass)
 };
 
 export default config;
